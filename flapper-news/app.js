46f273355832292616eabc8375e9fe4de1d6e4f9
CMilne <cmilne@ideo.com>
2016-02-09 11:27:06 -0500

    Intergrating Angular template with the DB backend

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -1,9 +1,16 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+mongoose.connect('mongodb://localhost/news');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -7,7 +7,12 @@ app.config(['$stateProvider','$urlRouterProvider',function($stateProvider, $urlR
 		.state('home', {
 			url: '/home',
 			templateUrl: '/home.html',
-			controller: 'MainCtrl'
+			controller: 'MainCtrl',
+			resolve: {
+				postPromise: ['posts', function(posts) {
+					return posts.getAll();
+				}]
+			}
 		})
 		.state('posts', {
 			url: '/posts/{id}',
@@ -25,27 +30,38 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
 	$scope.addPost = function() {
 		if(!$scope.title || $scope.title === '') { return; }
 
-		$scope.posts.push({
-			title: $scope.title, 
+		posts.create({
+			title: $scope.title,
 			link: $scope.link,
-			upvotes: 0,
-			comments: [
-				{author: 'Joe', body: 'Cool post!', upvotes: 0},
-				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-			]
 		});
 		$scope.title = '';
 		$scope.link = '';
 	};
 
 	$scope.incrementUpvotes = function(post) {
-		post.upvotes += 1;
+		posts.upvote(post);
 	};
 }]);
 
-app.factory('posts', [function() {
+app.factory('posts', ['$http', function($http) {
 	var o = {
 		posts: []
+	};
+	o.getAll = function() {
+		return $http.get('/posts').success(function(data) {
+			angular.copy(data, o.posts);
+		});
+	};
+	o.create = function(post) {
+		return $http.post('/posts', post).success(function(data) {
+			o.posts.push(data);
+		});
+	};
+	o.upvote = function(post) {
+		return $http.put('/post' + post._id + '/upvote')
+			.success(function(data) {
+				post.upvotes += 1;
+			});
 	};
 	return o;
 }]);
