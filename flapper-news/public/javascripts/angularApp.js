7588ec2e063bac9f187bc90b7f40a8774b9599f0
CMilne <cmilne@ideo.com>
2016-02-10 17:10:13 -0500

    Server works now and saves upvotes

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -17,7 +17,12 @@ app.config(['$stateProvider','$urlRouterProvider',function($stateProvider, $urlR
 		.state('posts', {
 			url: '/posts/{id}',
 			templateUrl: '/posts.html',
-			controller: 'PostsCtrl'
+			controller: 'PostsCtrl',
+			resolve: {
+				post: ['$stateParams', 'posts', function($stateParams, posts) {
+					return posts.get($stateParams.id);
+				}]
+			}
 		});
 	$urlRouterProvider.otherwise('home');
 }]);
@@ -52,29 +57,47 @@ app.factory('posts', ['$http', function($http) {
 			angular.copy(data, o.posts);
 		});
 	};
+	o.get = function(id) {
+		return $http.get('/posts/' + id).then(function(res) {
+			return res.data;
+		});
+	};
 	o.create = function(post) {
 		return $http.post('/posts', post).success(function(data) {
 			o.posts.push(data);
 		});
 	};
 	o.upvote = function(post) {
-		return $http.put('/post' + post._id + '/upvote')
+		return $http.put('/posts/' + post._id + '/upvote')
 			.success(function(data) {
 				post.upvotes += 1;
+			});
+	};
+	o.addComment = function(id, comment) {
+		return $http.post('/posts/' + id + '/comments', comment);
+	};
+	o.upvoteComment = function(post, comment) {
+		return $http.pust('/posts/' + post._.id + '/comments/' + comment._id + '/upvote')
+			.success(function(data) {
+				comment.upvotes += 1;
 			});
 	};
 	return o;
 }]);
 
-app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts) {
-	$scope.post = posts.posts[$stateParams.id];
+app.controller('PostsCtrl', ['$scope', 'posts', 'post', function($scope, posts, post) {
+	$scope.post = post;
 	$scope.addComment = function() {
 		if($scope.body === '') { return; }
-		$scope.post.comments.push({
+		posts.addComment(post._id, {
 			body: $scope.body,
 			author: 'user',
-			upvotes: 0
+		}).success(function(comment) {
+			$scope.post.comments.push(comment);
 		});
 		$scope.body = '';
+	};
+	$scope.incrementUpvotes = function(comment) {
+		posts.upvoteComment(post, comment);
 	};
 }]);
